3bd2a4ac2c975be36c194b30d5d64c0d70b8a68c
byron korir <byron.korir@student.moringaschool.com>
2023-10-06 13:57:56 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,18 +1,45 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  //looks through each item
+  for(i=0;i<array.length;i++){
+//looks through the rest of the item
+    for(j=i+1;j<array.length;j++){
+      //check if the addition === target 
+      if(array[i]+array[j]===target){
+        //returns true
+        return true
+      }
+        
+    }
+    
+  }
+  //else returns false
+  return false
+  
+
+
 }
 
 /* 
   Write the Big O time complexity of your function here
 */
+//quadratic time
+//it takes the first number and adds it to the rest of the numbers
+//and then checks it if equals to the target
 
 /* 
   Add your pseudocode here
 */
+//for (i=0;i<array.length;i++){
+//   for(j=i+1;)
+// }
 
 /*
   Add written explanation of your solution here
 */
+//looks through each item
+//looks through the rest of the item
+//check if the addition === target 
+//returns true
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
